fa232392237da245c6ef2501fe17ba589f8c1cba
Kim Nguyen <kimnivore@gmail.com>
2022-02-08 11:23:00 -0800

    completed post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -40,6 +40,29 @@ router.get('/:id', (req, res) => {
 })
 
 // 3 [POST] /api/posts
+router.post('/', (req, res) => {
+    if (!req.body.title || !req.body.contents){
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Posts.insert(req.body)
+            .then(({id}) => {
+                return Posts.findById(id)
+            })
+            .then(post => {
+            res.status(201).json(post);
+            })
+            .catch(error => {  
+            console.log(error);
+            res.status(500).json({
+                message: "There was an error while saving the post to the database"
+            })
+        })
+    }
+})
+
+
 // 4 [PUT] /api/posts/:id
 // 5 [DELETE] /api/posts/:id
 // 6 [GET] /api/posts/:id/comments
