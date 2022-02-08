2572cc6ebd06527af79255d5225f73626c14d093
Kim Nguyen <kimnivore@gmail.com>
2022-02-08 10:48:24 -0800

    completed get post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,7 +5,7 @@ const { Router } = require('express');
 const router = Router();
 
 
-
+// 1 [GET] /api/posts
 router.get('/', (req, res) => {
     console.log('posts router is working');
     Posts.find(req.query)
@@ -15,10 +15,34 @@ router.get('/', (req, res) => {
         .catch(error => {
             console.log(error);
             res.status(500).json({
-                message: "There was an error while saving the post to the database",
+                message: "The posts information could not be retrieved" 
             });
         });
 });
+// 2 [GET] /api/posts/:id
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if(post) {
+                res.status(200).json(post);
+            }else{
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "The post information could not be retrieved"
+            })
+        })
+})
+
+// 3 [POST] /api/posts
+// 4 [PUT] /api/posts/:id
+// 5 [DELETE] /api/posts/:id
+// 6 [GET] /api/posts/:id/comments
 
 
 module.exports = router;
