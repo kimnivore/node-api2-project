826be6820dc7324e7f6f2ac758a974adcfea87d6
Kim Nguyen <kimnivore@gmail.com>
2022-02-08 11:43:48 -0800

    completed put

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -62,8 +62,42 @@ router.post('/', (req, res) => {
     }
 })
 
-
 // 4 [PUT] /api/posts/:id
+router.put('/:id', (req, res) => {
+    const { title, contents } = req.body;
+    if(!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Posts.findById(req.params.id)
+            .then(id => {
+                if(!id){
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                    })
+                } else {
+                    return Posts.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if(data) {
+                    return Posts.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                if(post){
+                    res.json(post)
+                }
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).json({
+                    message: "The post information could not be modified"
+                })
+            })
+    }
+})
 // 5 [DELETE] /api/posts/:id
 // 6 [GET] /api/posts/:id/comments
 
