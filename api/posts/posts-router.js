3fd8bf3c5cd13a4359712a07fd5a80e3597656ef
Kim Nguyen <kimnivore@gmail.com>
2022-02-08 10:35:53 -0800

    completed get all posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,24 @@
 // implement your posts router here
+
+const Posts = require('./posts-model');
+const { Router } = require('express');
+const router = Router();
+
+
+
+router.get('/', (req, res) => {
+    console.log('posts router is working');
+    Posts.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "There was an error while saving the post to the database",
+            });
+        });
+});
+
+
+module.exports = router;
